7e37531935d5199add83ebd91077903943e6f1cc
Neel Rao <neelorao@gmail.com>
2025-02-08 20:22:12 -0800

    docs: remove unneeded mobile link

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -46,23 +46,12 @@ export default function Home() {
                 <PageTitle>colormotion</PageTitle>
                 <div className="space-y-4">
                     <Text>
-                        <Strong>colormotion</Strong>{' '}
-                        <span className="inline sm:hidden">
-                            (
-                            <TextLink
-                                href="https://gka.github.io/chroma.js/"
-                                target="_blank"
-                                rel="nofollow noreferrer"
-                            >
-                                GitHub
-                            </TextLink>
-                            )
-                        </span>
-                        is a JavaScript library of utilities for creating
-                        dynamic color palettes. Its primary use case is for
-                        generating color palettes that change over time, such as
-                        for animations or visualizations in LED art. It is built
-                        on top of the fantastic{' '}
+                        <Strong>colormotion</Strong> is a JavaScript library of
+                        utilities for creating dynamic color palettes. Its
+                        primary use case is for generating color palettes that
+                        change over time, such as for animations or
+                        visualizations in LED art. It is built on top of the
+                        fantastic{' '}
                         <TextLink
                             href="https://gka.github.io/chroma.js/"
                             target="_blank"
